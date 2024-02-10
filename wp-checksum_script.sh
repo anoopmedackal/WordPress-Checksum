ee8a6d7aaaff3112473e374a700945a1d7c96552
anoopmedackal <102291104+anoopmedackal@users.noreply.github.com>
2024-02-10 09:50:46 +0530

    Updated wp-checksum_script.sh

--- wp-checksum_script.sh
+++ wp-checksum_script.sh
@@ -13,6 +13,34 @@ if ! id -u "$cpuser" >/dev/null 2>&1; then
   exit 1
 fi
 
+function wp-checksum-with-file-removal() {
+su - $cpuser -s /bin/bash << 'EOF'
+cpuser=$(whoami)
+for docroot in $(find "/home/$cpuser/" -type d -iname 'wp-content' | xargs dirname)
+do
+        cd $docroot
+        version="$(grep -s '^\$wp_version' "wp-includes/version.php" |cut -d\' -f2)"
+        echo -e "\e[31mRemoving unwanted WP files from $docroot\e[0m"
+        wp core verify-checksums 2>&1 | grep 'File should not exist' | awk '{print $6}'|xargs rm -fv
+        echo -e "\e[31mRunning WP checksum\e[0m"
+        wp core verify-checksums
+done
+EOF
+}
+
+function wp-checksum() {
+su - $cpuser -s /bin/bash << 'EOF'
+cpuser=$(whoami)
+for docroot in $(find "/home/$cpuser/" -type d -iname 'wp-content' | xargs dirname)
+do
+        cd $docroot
+        version="$(grep -s '^\$wp_version' "wp-includes/version.php" |cut -d\' -f2)"
+        echo -e "\e[31mDownloading WP core files of version $version in $docroot\e[0m"
+        wp core download --force --version=$version
+done
+EOF
+}
+
 
 # cPanel user to check
 su - $cpuser -s /bin/bash << 'EOF' 
@@ -34,37 +62,24 @@ done
 EOF
 
 
-function wp-checksum() {
-su - $cpuser -s /bin/bash << 'EOF'
-cpuser=$(whoami)
-for docroot in $(find "/home/$cpuser/" -type d -iname 'wp-content' | xargs dirname)
-do
-	cd $docroot
-       	version="$(grep -s '^\$wp_version' "wp-includes/version.php" |cut -d\' -f2)"
-	echo -e "\e[31mRemoving unwanted WP files from $docroot\e[0m"
-        wp core verify-checksums 2>&1 | grep 'File should not exist' | awk '{print $6}'|xargs rm -fv
-	echo -e "\e[31mDownloading WP core files of version $version\e[0m"   
-        wp core download --force --version=$version
-	echo -e "\e[31mRunning WP checksum\e[0m"
-        wp core verify-checksums
-done 
-EOF
-}
-
 if [ -s "/home/$cpuser/wp-checklist.txt" ]; then
 	read -p $'\e[1;31mDo you want this script to delete all the files listed above and download WP core files in each WP installations?\e[0m (yes/no): ' answer
 	answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
 	if [[ $answer == "yes" ]]; then
+		wp-checksum-with-file-removal
 		wp-checksum
 		rm -f "/home/$cpuser/wp-checklist.txt"
 	elif [[ $answer == "no" ]]; then
+		wp-checksum		
 		rm -f "/home/$cpuser/wp-checklist.txt"
 		exit 0
 	else
 		rm -f "/home/$cpuser/wp-checklist.txt"
 		echo "Invalid input. Please enter 'yes' or 'no'."
+		wp-checksum
 		exit 1
 	fi
 else
+	wp-checksum
 	exit 0
 fi
